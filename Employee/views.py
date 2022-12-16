ea46a9a25f8431c27138c159a706a170a5afcf63
Keerthana-Rajendran <103659988+Keerthana-Rajendran@users.noreply.github.com>
2022-12-16 10:32:11 +0530

    Add files via upload

---
+++ Employee/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from Employee.models import Employee
+from Employee.forms import EmployeeForm
+
+
+def employee_register(request):
+    empDict = {
+        'empForm': EmployeeForm()
+    }
+    if request.method == 'POST':
+        empForm = EmployeeForm(request.POST)
+        if empForm.is_valid():
+            empForm.save(commit=True)
+        else:
+            empDict = {
+                'empForm': empForm
+            }
+            return render(request, 'Employee/add_employee.html', context = empDict)
+    return render(request, 'Employee/add_employee.html', context = empDict)
+
+
+
+def employee_view(request):
+    employees = Employee.objects.all().filter()
+    return render(request,'Employee/employee.html', {'employees': employees})
+
+
+def delete_employee(request, pk):
+    employees = Employee.objects.get(id = pk)
+    employees.delete()
+    return employee_view(request)
+
+def update_employee(request, pk):
+
+    employee = Employee.objects.get(id = pk)
+    employeeForm = EmployeeForm(instance = employee)
+    employee_dict = {
+        'empForm':employeeForm
+    }
+
+    if request.method=='POST':
+        employeeForm = EmployeeForm(request.POST, instance = employee)
+
+        if employeeForm.is_valid():
+            employee = employeeForm.save(commit=True)
+            employee.save()
+        else:
+            
+            employee_dict = {
+                'empForm': employeeForm
+            }
+            return render(request, 'Employee/update_employee.html', context = employee_dict)
+            
+    return render(request, 'Employee/update_employee.html', context = employee_dict)
+
+
+def search_employee_view(request):
+        
+    employees = ""
+    
+    if request.GET.get("query"):
+        employees = Employee.objects.filter(Eid = request.GET.get("query"))
+    return render(request, 'Employee/employee.html', {'employees': employees})
+
